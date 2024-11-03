35999000d0ad8dc5793eddc7009a75519fa54c4f
Soon Chai <256in4@gmail.com>
2024-11-03 21:49:49 +0300

    feat: ability to toggle hoverlight effects applied

--- src/src/mixins/hoverlight.ts
+++ src/src/mixins/hoverlight.ts
@@ -8,13 +8,13 @@ export class Hoverlightable {
 		this.on(
 			'mouseover',
 			function (e: any) {
+				if (this._hoverlightDisabled) return
 				var filters = this._applyHoverOn.filters()
 				if (!filters) filters = []
 				filters.push(filter)
 				this._applyHoverOn.filters(filters)
 				this._applyHoverOn.cache({ pixelRation: 5 })
 				this._applyHoverOn.brightness(0.3)
-				
 			},
 			this
 		)
@@ -28,15 +28,30 @@ export class Hoverlightable {
 				this._applyHoverOn.filters(filters)
 				this._applyHoverOn.clearCache()
 				this._applyHoverOn.cache({ pixelRation: 5 })
-				
 			},
 			this
 		)
 	}
+
 	_applyHoverOn: any
-	_parentGroupHoverOn:any
-	setFiltersApplyable(obj: any, parent: any = null) {
+	_parentGroupHoverOn: any
+	_hoverlightDisabled: boolean = false
+	_disableAvailable: boolean
+	setFiltersApplyable(
+		obj: any,
+		parent: any = null,
+		disablingAvailable: boolean = false
+	) {
 		this._applyHoverOn = obj
 		this._parentGroupHoverOn = parent
+		this._disableAvailable = disablingAvailable
+	}
+
+	disableHoverlighting() {
+		if (this._disableAvailable) this._hoverlightDisabled = true
+	}
+
+	enableHoverlighting() {
+		if (this._disableAvailable) this._hoverlightDisabled = false
 	}
 }
